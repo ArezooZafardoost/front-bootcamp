eeb96dbb73514e18819c56b8e95ef8dce094f226
ArezooZafardoost <arezoo.zafardoost@gmail.com>
2023-09-06 21:39:28 +0330

    js6.js

---
+++ q2.js
@@ -0,0 +1,24 @@
+//avaaal
+// var rect=document.getElementById("rect").style;
+
+// document.onmousemove=function(event){
+
+// var x=event.clientX;
+// var y=event.clientY;
+// rect.left=x+'px';
+// rect.top=y+'px';
+
+// }
+
+//dooovom
+
+function My_Function() {
+    document.body.style.backgroundColor = "blue";
+}
+
+
+
+
+
+
+
